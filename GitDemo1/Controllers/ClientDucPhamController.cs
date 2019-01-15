674c7b616fd49fc0c0bc274c1fddf185584c08d8
Phạm Minh Đức <42537338+pmduc97@users.noreply.github.com>
2019-01-15 15:53:21 +0700

    ClientDucPham Create

---
+++ GitDemo1/Controllers/ClientDucPhamController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GitDemo1.Controllers
+{
+    public class ClientDucPhamController : Controller
+    {
+        // GET: ClientDucPham
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
