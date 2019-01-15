65e4324578f8d3a62ea10a8f5f4ad517d7273650
Phạm Minh Đức <42537338+pmduc97@users.noreply.github.com>
2019-01-15 22:08:26 +0700

    TestPull
    DucPham TestPullRequest

---
+++ GitDemo1/Controllers/TestPullRequestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GitDemo1.Controllers
+{
+    public class TestPullRequestController : Controller
+    {
+        // GET: TestPullRequest
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
