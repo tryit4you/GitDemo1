af6d5b81cebceef0815e05e3280facaffdf576dc
Phạm Minh Đức <42537338+pmduc97@users.noreply.github.com>
2019-01-16 08:26:46 +0700

    New Controller TestNgay2

---
+++ GitDemo1/Areas/DucArea/Controllers/TestNgay2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GitDemo1.Areas.DucArea.Controllers
+{
+    public class TestNgay2Controller : Controller
+    {
+        // GET: DucArea/TestNgay2
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
